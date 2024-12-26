e9fc704dad6f30877e951f8f841789e690f3ffcc
almusavveer <almusavveerjamadar@gmail.com>
2024-12-26 17:34:59 +0530

    Added env variable

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,6 +1,6 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai"; 
 
-const genAI = new GoogleGenerativeAI("AIzaSyCmzV1YPhk1YLliWTKZ0q9aRuslFNYlFyg");
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 
 const model = genAI.getGenerativeModel({
   model: "gemini-2.0-flash-exp",
@@ -13,25 +13,24 @@ const commonGenerationConfig = {
   responseMimeType: "text/plain",
 };
 
-
 async function run(prompt, options = {}) {
   // default values
-  const { temperature=0.7, lengthType='medium' } = options;
+  const { temperature = 0.7, lengthType = "medium" } = options;
   let modifiedPrompt = prompt;
 
   // Add modifiers based on lengthType
-  switch(lengthType){
-    case 'short':
-     modifiedPrompt = `Give a short and concise answer for this prompt: ${prompt}`;
-     break;
-    case 'medium':
-      modifiedPrompt = `Give a medium length answer of about 100 words for this prompt: ${prompt}`
-    break;
-     case 'long':
-      modifiedPrompt = `Provide a full and detailed answer for this prompt: ${prompt}`
-     break;
+  switch (lengthType) {
+    case "short":
+      modifiedPrompt = `Give a short and concise answer for this prompt: ${prompt}`;
+      break;
+    case "medium":
+      modifiedPrompt = `Give a medium length answer of about 100 words for this prompt: ${prompt}`;
+      break;
+    case "long":
+      modifiedPrompt = `Provide a full and detailed answer for this prompt: ${prompt}`;
+      break;
     default:
-      modifiedPrompt =  `Give a short and concise answer for this prompt: ${prompt}`
+      modifiedPrompt = `Give a short and concise answer for this prompt: ${prompt}`;
   }
 
   const generationConfig = {
@@ -39,21 +38,18 @@ async function run(prompt, options = {}) {
     temperature: temperature,
   };
 
-
   const chat = model.startChat({
     generationConfig,
     history: [],
   });
 
-
-  try{
-      const result = await chat.sendMessage(modifiedPrompt);
-      return result.response.text()
-  }catch(error){
-      console.log(`Error: ${error}`)
-      return "Something went wrong. Please try again later."
+  try {
+    const result = await chat.sendMessage(modifiedPrompt);
+    return result.response.text();
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return "Something went wrong. Please try again later.";
   }
 }
-
 
 export default run;
